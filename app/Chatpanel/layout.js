00a714c9560e9554738d45512d59fb3633f570e8
Charchit Sharma <sharmacharchit19@gmail.com>
2026-03-26 11:25:42 +0530

    sixth commit

--- app/Chatpanel/layout.js
+++
@@ -1,7 +0,0 @@
-export default function ChatPanelLayout({ children }) {
-  return (
-    <div>
-      {children}
-    </div>
-  );
-}
--- pages/SuperAdmin/layout.js
+++
@@ -1,7 +0,0 @@
-export default function SuperAdminLayout({ children }) {
-  return (
-    <div>
-      {children}
-    </div>
-  );
-}
